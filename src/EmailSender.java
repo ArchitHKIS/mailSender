25db7abc934de071525feb26ba08a7e3c1d46799
“Archit <190902@hkis.edu.hk>
2016-12-02 12:52:02 +0800

    Formatted code
    :)

--- src/EmailSender.java
+++ src/EmailSender.java
@@ -1,5 +1,4 @@
 import java.util.Properties;
-
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -8,7 +7,7 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
 public class EmailSender {
-	public static boolean sendMail(String from, String password, String message, String to[]){
+	public static boolean sendMail(String from, String password, String message, String to[]) {
 		String host = "smtp.gmail.com";
 		Properties props = System.getProperties();
 		props.put("mail.smtp.starttls.enable", "true");
@@ -19,24 +18,24 @@ public class EmailSender {
 		props.put("mail.smtp.auth", true);
 		Session session = Session.getDefaultInstance(props, null);
 		MimeMessage mimeMessage = new MimeMessage(session);
-		try{
+		try {
 			mimeMessage.setFrom(new InternetAddress(from));
 			InternetAddress[] toAddress = new InternetAddress[to.length];
-			for(int i = 0; i<to.length; i++){
+			for (int i = 0; i < to.length; i++) {
 				toAddress[i] = new InternetAddress(to[i]);
 			}
-			for(int i = 0; i<toAddress.length;i++){
-			mimeMessage.addRecipient(RecipientType.TO, toAddress[i]);
+			for (int i = 0; i < toAddress.length; i++) {
+				mimeMessage.addRecipient(RecipientType.TO, toAddress[i]);
 			}
-			
+
 			mimeMessage.setSubject("mail using javamail api");
 			mimeMessage.setText(message);
 			Transport transport = session.getTransport("smtp");
-			transport.connect(host,from,password);
+			transport.connect(host, from, password);
 			transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
 			transport.close();
 			return true;
-		}catch(MessagingException me){
+		} catch (MessagingException me) {
 			me.printStackTrace();
 		}
 		return false;
--- src/GoogleMail.java
+++ src/GoogleMail.java
@@ -1,14 +1,15 @@
 
-public class GoogleMail{
-	public static void main(String[] args){
+public class GoogleMail {
+	public static void main(String[] args) {
 		long startTime = System.nanoTime();
-		String[] to = {"190902@hkis.edu.hk","architksrivastava@gmail.com"};
-		if(EmailSender.sendMail("190902@hkis.edu.hk", "passwordshouldbereplaced", "message that was sent form eclipse", to)){
+		String[] to = { "190902@hkis.edu.hk", "architksrivastava@gmail.com" };
+		if (EmailSender.sendMail("190902@hkis.edu.hk", "passwordshouldbereplaced", "message that was sent form eclipse",
+				to)) {
 			System.out.println("sent message");
-		}else{
+		} else {
 			System.out.println("nah");
 		}
 		long endTime = System.nanoTime();
-		System.out.println("Took "+(endTime - startTime) + " ns"); 
+		System.out.println("Took " + (endTime - startTime) + " ns");
 	}
 }
